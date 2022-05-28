42152ecca316846b736c54b1dda28c87e4baa9ab
AngelPerz <aperezgomez55@gmail.com>
2022-05-28 13:28:15 +0200

    application skeleton

---
+++ iobuilders-users-boot/src/main/java/com/angelperez/iobuildersusers/IobuildersUsersApplication.java
@@ -0,0 +1,13 @@
+package com.angelperez.iobuildersusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IobuildersUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IobuildersUsersApplication.class, args);
+	}
+
+}
---
+++ iobuilders-users-boot/src/test/java/com/angelperez/iobuildersusers/IobuildersUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelperez.iobuildersusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IobuildersUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
